957322bdf427220cf9aa4b25a5f1aebcd097b5ac
patynara <patriciasinara.pmvc@gmail.com>
2024-10-30 23:33:15 -0300

    Descrição das alterações

---
+++ scripts/config.js
@@ -0,0 +1,1 @@
+export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzEPx4T8zPdCQY4I2P3KQCUDhpGEUYlQkpKf1ySMdQ1ku2T2mGDdKvM2Ppd524tfsXo/exec';
--- scripts/detailsControl.js
+++ scripts/detailsControl.js
@@ -1,6 +1,7 @@
-// URL do Web App do Google Apps Script
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzEPx4T8zPdCQY4I2P3KQCUDhpGEUYlQkpKf1ySMdQ1ku2T2mGDdKvM2Ppd524tfsXo/exec';
+import { APPS_SCRIPT_URL } from './config.js';
 
+
+// Resto do código permanece igual
 function getQueryParams() {
   const params = {};
   const search = window.location.search.substring(1);
